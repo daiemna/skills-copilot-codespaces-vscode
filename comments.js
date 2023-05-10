cfe8b3d3d85954e7a1a762cbd913b021e16258bb
daiemnation <daiem.dna@gmail.com>
2023-05-10 12:38:52 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,27 @@
+// Ceate web server
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+const uuid = require('uuid/v4');
+const fs = require('fs');
+const app = express();
+
+app.use(morgan('tiny'));
+app.use(bodyParser.json());
+app.use(cors());
+
+const comments = require('./comments.json');
+
+// GET /comments
+app.get('/comments', (req, res) => {
+  res.json(comments);
+});
+
+// POST /comments
+app.post('/comments', (req, res) => {
+  const comment = {
+    id: uuid(),
+    timestamp: Date.now(),
+    body: req.body.body,
